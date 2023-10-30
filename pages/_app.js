981a5a43249bbaf487c189b98f76338e0dd690ef
Lennart Kaminsky <lennartkaminsky@googlemail.com>
2023-10-30 11:52:11 +0100

    removed comments

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,10 @@
 import GlobalStyle from "../styles";
-// import { SWRConfig } from "swr";
-
-// const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      {/* <SWRConfig
-        value={{
-          fetcher,
-          refreshInterval: 5000,
-        }}
-      > */}
       <Component {...pageProps} />
-      {/* </SWRConfig> */}
     </>
   );
 }
